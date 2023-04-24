1dcc1b03dddbde906d0d09ff511126a270a0f74a
Jacie1 <irinemwanga5@gmail.com>
2023-04-24 10:26:38 +0000

    web flask tasks

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,15 +1,24 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+"""
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-@app.route('/')
-def hello():
-    return 'Hello HBNB!'
 
-@app.route('/hbnb')
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    return 'HBNB'
+    """Displays 'HBNB'."""
+    return "HBNB"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
+
